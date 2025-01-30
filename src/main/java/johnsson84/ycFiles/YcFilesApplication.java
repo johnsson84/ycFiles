821cf05f54ea3921be14cffc63eb062a764cfb04
Johan Johnsson <johan@johnsson-net.se>
2025-01-31 00:10:57 +0100

    init

---
+++ src/main/java/johnsson84/ycFiles/YcFilesApplication.java
@@ -0,0 +1,13 @@
+package johnsson84.ycFiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YcFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/johnsson84/ycFiles/YcFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package johnsson84.ycFiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YcFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
